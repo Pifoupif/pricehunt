815754564abae259b33ccd3ec0c9239e83b2b4ba
pierre-francois <pifoupif@hotmail.com>
2019-02-25 18:14:21 +0100

    main routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :product, only: [:show] do
+    resources :alert, only: [:new, :create]
+  end
+  resources :alert, only: [:update, :edit, :destroy, :index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
