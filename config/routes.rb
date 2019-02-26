48fc862ea0cea812c4584bbff2ffb94df939f49c
Antonin Gury-Coupier <antonin_gb@hotmail.fr>
2019-02-26 16:40:48 +0100

    creation index alerts

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :alert, only: [:new, :create]
   end
 
-  resources :alert, only: [:update, :edit, :destroy, :index, :show]
+  resources :alerts, only: [:update, :edit, :destroy, :index, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
