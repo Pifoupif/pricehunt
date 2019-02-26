ff468d1a3ca44c268abb131b2cf018cf55aa272d
Guillaume de Montecler <guillaume.demontecler@gmail.com>
2019-02-26 12:15:19 +0100

    en cours

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,7 @@
 class OffersController < ApplicationController
+  def index
+    @offers = Offer.all
+    @product = Product.find(params[:id])
+    @product_offer = @offers.where(@product.id = @offers.product_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
     resources :alert, only: [:new, :create]
   end
   resources :alert, only: [:update, :edit, :destroy, :index, :show]
+
+  resources :product, only: [:show] do
+    resources :offers, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
