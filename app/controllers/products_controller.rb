898aa8e25880350258c1a18a386e646bbdf10099
Antonin Gury-Coupier <antonin_gb@hotmail.fr>
2019-02-27 16:05:21 +0100

    button change color

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,13 @@
 class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
-    @offers = @product.offers
+    @offers = @product.offers.joins(:retailer).order('retailers.rating ASC')
+    @criteria = 'rating'
+    if params[:sort_by_price]
+      @offers = @product.offers.joins(:prices).order('prices.price ASC LIMIT 1')
+    @criteria = 'price'
+    end
+    raise
     @alert = Alert.new
   end
 end
