1c23444d78ba35df77e66679e91735e3c5a665ab
pierre-francois <pifoupif@hotmail.com>
2019-02-27 13:08:19 +0100

    wip

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -3,10 +3,19 @@ class AlertsController < ApplicationController
     @alert = Alert.new(alert_params)
     @alert.user = current_user
     @alert.product_id = params[:product_id]
+    raise
     if @alert.save
-      redirect_to product_path(params[:product_id])
+      respond_to do |format|
+        format.html { redirect_to product_path(params[:product_id]) }
+        format.js
+      end
     else
-      raise
+      @product = Product.find(params[:product_id])
+      @offers = @product.offers
+      respond_to do |format|
+      format.html { render 'products/show' }
+      format.js
+      end
     end
   end
 
@@ -30,7 +39,7 @@ class AlertsController < ApplicationController
 private
 
   def alert_params
-    params.require(:alert).permit(:target_price)
+    params.require(:alert).permit(:target_price, :by_email, :by_text_message)
   end
 
 end
--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,10 +1,18 @@
 class Alert < ApplicationRecord
+  include ActiveModel::Validations
+  validates_with AlertPresenceValidator
+
   belongs_to :product
   belongs_to :user
 
   validates :target_price, presence: true
+  validates :by_text_message, presence: true
+  validates :by_email, presence: true
+  #validates :by_email, inclusion: { in: [true],  unless: Proc.new { |a| a.by_text_message? }}
+  #validates :by_text_message, inclusion: { in: [true], unless: Proc.new { |a| a.by_email? }}
 
   after_create :send_alert_email
+
 
   private
 
