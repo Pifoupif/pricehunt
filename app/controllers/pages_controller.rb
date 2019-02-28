6e1bdeb7b4603124f7c6d51e1ba0d659a671254e
pierre-francois <pifoupif@hotmail.com>
2019-02-28 12:04:54 +0100

    search function ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     @home = ""
+    @product = Product.where("name ILIKE ?", "%#{params[:query]}%").first
 
     if params[:query].present?
       sql_query = "products.name @@ :query"
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,27 @@
 class ProductsController < ApplicationController
   def show
-    @product = Product.find(params[:id])
-    @offers = @product.offers.joins(:retailer).order('retailers.rating DESC')
-    @filter = false
-    if params[:sort_by_price]
-      @filter = true
-      @offers = @product.offers.joins(:prices).order('prices.price ASC')
+    if params[:query]
+      @product = Product.where("name ILIKE ?", "%#{params[:query]}%").first
+      redirect_to product_path(@product)
+    else
+      @product = Product.find(params[:id])
+      @offers = @product.offers.joins(:retailer).order('retailers.rating DESC')
+      @filter = false
+      if params[:sort_by_price]
+        @filter = true
+        @offers = @product.offers.joins(:prices).order('prices.price ASC')
+      end
+      @alert = Alert.new
     end
-    @alert = Alert.new
   end
+
+  def search_show
+    @product = Product.where(name: params[:query]).first
+    redirect_to product_path(@product)
+  end
+
 end
+
+
+
+Product.where("name ILIKE ?", "%STAN%").first
