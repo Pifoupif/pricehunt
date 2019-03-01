65105d2cf427b66b13f1ead9a35665b915fd71de
pierre-francois <pifoupif@hotmail.com>
2019-03-01 12:28:32 +0100

    search bar working but error in url

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,27 +1,31 @@
 class ProductsController < ApplicationController
   def show
+    @alert = Alert.new
     if params[:query]
-      #@product = Product.find(denich_id: params[:query])
-      @product = Product.where("denich_id ILIKE ?", params[:query])
-      #@product = Product.where("denich_id ILIKE ?", "%#{params[:query]}%").first
-
-      redirect_to product_path(@product)
+      @product = Product.find_by(denich_id: params[:query])
+      @offers = @product.offers.joins(:prices).order('prices.price ASC')
     else
       @product = Product.find(params[:id])
       @offers = @product.offers.joins(:retailer).order('retailers.rating DESC')
-      @filter = false
-      if params[:sort_by_price]
-        @filter = true
-        @offers = @product.offers.joins(:prices).order('prices.price ASC')
-      end
-      @alert = Alert.new
     end
+
+    # if params[:query]
+    #   #@product = Product.find(denich_id: params[:query])
+    #   @product = Product.where("denich_id ILIKE ?", params[:query])
+    #   #@product = Product.where("denich_id ILIKE ?", "%#{params[:query]}%").first
+    #   redirect_to product_path(@product)
+    # else
+    #   @filter = false
+    #   if params[:sort_by_price]
+    #     @filter = true
+    #   end
+    # end
   end
 
-  def search_show
-    @product = Product.where(name: params[:query]).first
-    redirect_to product_path(@product)
-  end
+  # def search_show
+  #   @product = Product.where(name: params[:query]).first
+  #   redirect_to product_path(@product)
+  # end
 
 end
 
