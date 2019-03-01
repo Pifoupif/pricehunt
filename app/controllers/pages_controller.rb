de20812764db211b1c568e321a42b095961331b3
pierre-francois <pifoupif@hotmail.com>
2019-03-01 11:08:30 +0100

    no error but search not working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   def home
     @home = ""
     @product = Product.where("name ILIKE ?", "%#{params[:query]}%").first
-
+    #@product = Product.where(denich_id: params[:query])
+    #@product = Product.find("denich_id ILIKE ?", "%#{params[:query]}%")
     if params[:query].present?
       sql_query = "products.denich_id @@ :query"
       @products = Product.where(sql_query, query: "%#{params[:query]}%")
