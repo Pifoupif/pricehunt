7996d29a075eb19e25a6402964121cbbce81226e
Antonin Gury-Coupier <antonin_gb@hotmail.fr>
2019-03-04 16:04:19 +0100

    picture results

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,4 @@
 class ProductsController < ApplicationController
-<<<<<<< HEAD
    def search
       @product = Product.first # for the searchbar
 
@@ -23,38 +22,15 @@ class ProductsController < ApplicationController
       response = open(denicheur_url).read
       data = JSON.parse(response)
 
-      item = data['message']['product']['items'].first
-      redirect_to product_path(@product, query: item['id'])
+      items = data['message']['product']['items']
+      # redirect_to product_path(@product, query: item['id'])
+      redirect_to search_results_products_path(items: items)
     end
 
-=======
-  def search
-    @product = Product.first # for the searchbar
-
-    # Google Vision
-    image_annotator = Google::Cloud::Vision::ImageAnnotator.new
-    # file_name = File.new(open('app/assets/images/Stan-smith.jpg'))
-    file_name = params[:photo].path
-    response = image_annotator.web_detection image: file_name
-
-    @descriptions = []
-    response.responses.each do |res|
-      res.web_detection.web_entities.each do |entity|
-        @descriptions << entity.description
-      end
+    def search_results
+      @results = params[:items]
     end
 
-    # Denicheur
-    keyword = @descriptions.first
-
-    denicheur_url = "https://search.ledenicheur.fr/classic?class=Search_Supersearch&method=search&market=fr&skip_login=1&modes=product,raw_sorted,raw&limit=12&q=#{keyword}"
-    response = open(denicheur_url).read
-    data = JSON.parse(response)
-
-    item = data['message']['product']['items'].first
-    redirect_to product_path(@product, query: item['id'])
-  end
->>>>>>> eeb802ff40bbf415e9d14c351204ddc32e4557e3
 
   def show
     @alert = Alert.new
