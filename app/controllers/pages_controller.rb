c13e4533b77c3a376238b928140df8a4dd836bdb
Antonin Gury-Coupier <antonin_gb@hotmail.fr>
2019-03-04 12:48:41 +0100

    link uploader to API

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,16 +11,5 @@ class PagesController < ApplicationController
     else
       @products = Product.all
     end
-
-    image_annotator = Google::Cloud::Vision::ImageAnnotator.new
-    file_name = File.new(open('app/assets/images/Stan-smith.jpg'))
-    response = image_annotator.web_detection image: file_name
-
-    @descriptions = []
-    response.responses.each do |res|
-      res.web_detection.web_entities.each do |entity|
-        @descriptions << entity.description
-      end
-    end
   end
 end
