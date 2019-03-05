a406cb87fb7c0a9aa1ca1f2f16fe752b8c99254d
pierre-francois <pifoupif@hotmail.com>
2019-03-05 18:51:45 +0100

    wip

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,6 @@
 import "bootstrap";
 import { button_sort } from '../components/button_price';
 import { initSelect2 } from '../components/init_select2';
-import { initSelect2Nav } from '../components/init_select2_navbar';
 import 'select2/dist/css/select2.css';
 import { initChart } from '../components/chart.js';
 
@@ -11,13 +10,5 @@ if (document.querySelector(".sort-price")) {
   button_sort();
 }
 
-if (document.querySelector("#select2-navbar")) {
-  initSelect2Nav();
-}
-
-if (document.querySelector(".searchbar-position")) {
-  initSelect2();
-}
-
 initChart();
-
+initSelect2();
