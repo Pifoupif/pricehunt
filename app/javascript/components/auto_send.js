7c6927b7c25bc43cb6adb982a0fabdcce7df8189
pierre-francois <pifoupif@hotmail.com>
2019-03-07 10:43:59 +0100

    photo search ok

---
+++ app/javascript/components/auto_send.js
@@ -0,0 +1,22 @@
+function submitOnChange() {
+    document.querySelector('input[id="photo"]').onchange=submitForm;
+};
+
+function submitForm(event) {
+  document.forms["photo-form"].submit()
+};
+
+export { submitOnChange };
+
+
+// window.onload = function () {
+//   var auto = setTimeout(function(){ autoRefresh(); }, 100);
+//   function submitform(){
+//     document.forms["photo-form"].submit();
+//   };
+//   function autoRefresh(){
+//     clearTimeout(auto);
+//     auto = setTimeout(function(){ submitform(); autoRefresh(); }, 10000);
+//   };
+// };
+
